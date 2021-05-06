f5bf6c093d590218bbacc7a1db9a4035d3bdc3c9
Eliu <supergonzalez360@gmail.com>
2021-05-05 22:22:50 -0700

    Implementado Firebase

---
+++ app/src/main/java/eliu/gonzalez/mydigimind/ContrasenaActivity.kt
@@ -0,0 +1,46 @@
+package eliu.gonzalez.mydigimind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_contrasena.*
+
+class ContrasenaActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contrasena)
+
+        auth = Firebase.auth
+
+
+        btn_restablecer.setOnClickListener {
+            val et_correo: EditText = findViewById(R.id.et_correo_cont)
+
+            var correo: String = et_correo.text.toString()
+
+            if (!correo.isNullOrBlank()) {
+                //enviarcorreo
+                auth.sendPasswordResetEmail(correo)
+                    .addOnCompleteListener { task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(this,"Se envio un correo a ${correo}",Toast.LENGTH_SHORT).show()
+                        }else{
+                            Toast.makeText(this,"Error al enviar correo",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(
+                    this, "Ingresar correo",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    }
+}
